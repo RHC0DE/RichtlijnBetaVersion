e52dc35c3282f2f890a3626bfe511d91adc82de8
R H <96940361+RHC0DE@users.noreply.github.com>
2022-07-13 11:55:15 +0200

    Initial Commit

---
+++ RichtlijnBetaVersion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RichtlijnBetaVersion
+//
+//  Created by Guest User on 13/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RichtlijnBetaVersion/RichtlijnBetaVersionApp.swift
@@ -0,0 +1,17 @@
+//
+//  RichtlijnBetaVersionApp.swift
+//  RichtlijnBetaVersion
+//
+//  Created by Guest User on 13/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct RichtlijnBetaVersionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
